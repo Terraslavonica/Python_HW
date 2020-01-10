2cd9b74d739d27cc7a5575b1916c24478c9aa368
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-01-10 16:27:14 +0300

    Not full yet

--- HW_11.py
+++ HW_11.py
@@ -1,10 +1,55 @@
+import numpy as np
+import random
+import time
+import matplotlib.pyplot as plt
+
+
 ## Task 1. Создайте 3 эррея различными способами (5 баллов)
+ar1 = np.array([11, 21, 32, 43, 54, 65], dtype=np.int8)
+print(ar1)
+
+ar2 = np.full((3, 2), 'Hello world')
+print(ar2)
+
+ar3 = np.full((2, 5, 5), 69)
+print(ar3)
+
+ar4 = np.arange(66, 666, 66)
+print(ar4)
 
 
 ## Task 2. Замерьте время вычисления чисел от 0 до 1 из равномерного распределения с помощью модулей random и numpy,
 # изобразите зависимость времени вычисления от количества вычисляемых чисел для них. Другими словами - по x идёт то,
 # сколько чисел за прогон вы взяли от 0 до 1, а по y - время, которое это заняло для random и numpy (10 баллов)
+##1
+timeran = []
+numbran = []
+for i in range(1000, 1000000, 10000):
+    numbran.append(i)
+    start = time.time()
+    for j in range(i):
+        random.random()
+    stop = time.time()
+    timeran.append(stop - start)
+print(timeran)
+len(timeran)
+plt.scatter(numbran, timeran, c="red")
 
+##2
+timenp = []
+numbnp = []
+for i in range(1000, 1000000, 10000):
+    numbnp.append(i)
+    start = time.time()
+    npr = np.random.uniform(low=0.0, high=1.0, size=i)
+    stop = time.time()
+    timenp.append(stop - start)
+print(timenp)
+len(timenp)
+plt.scatter(numbnp, timenp, c="blue")
+
+## Сделать функцией?
+## наверное для модуля рандома как-то иначе нужно, что я пока не знаю как иначе, чем петлей
 
 # Task 3. Сделайте функцию для проверки является ли список отсортированным (без использования sorted или sort).
 # Затем реализуйте monkey sort, а потом визуализируйте следующее: распределение времени работы алгоритма от размера
