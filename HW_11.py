95a0e3d5e515bf8bca7b2e95aa92e2b735787e5d
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-01-11 02:09:20 +0300

    Not full yet :(

--- HW_11.py
+++ HW_11.py
@@ -31,9 +31,9 @@ for i in range(1000, 1000000, 10000):
         random.random()
     stop = time.time()
     timeran.append(stop - start)
-print(timeran)
-len(timeran)
-plt.scatter(numbran, timeran, c="red")
+
+plt.figure(figsize=(12, 8))
+plt.scatter(numbran, timeran, c="red", label='random')
 
 ##2
 timenp = []
@@ -44,25 +44,99 @@ for i in range(1000, 1000000, 10000):
     npr = np.random.uniform(low=0.0, high=1.0, size=i)
     stop = time.time()
     timenp.append(stop - start)
-print(timenp)
-len(timenp)
-plt.scatter(numbnp, timenp, c="blue")
+
+plt.scatter(numbnp, timenp, c="blue", label='numpy')
+plt.legend()
 
 ## Сделать функцией?
 ## наверное для модуля рандома как-то иначе нужно, что я пока не знаю как иначе, чем петлей
+
 
 # Task 3. Сделайте функцию для проверки является ли список отсортированным (без использования sorted или sort).
 # Затем реализуйте monkey sort, а потом визуализируйте следующее: распределение времени работы алгоритма от размера
 # сортируемого списка. То есть по x идёт размер массива, а по y - среднее время нескольких прогонов и их отклонение
 # (или дисперсия) (10 баллов)
 
+def is_sorted(data):
+    """
+    Determine whether the data is sorted.
+    :param data: list - data that mast be sorted
+    :return: True if the data is sorted and False if not
+    """
+    for i in range(len(data) - 1):
+    if data[i] > data[i + 1]:
+        return False
+    else:
+        return True
+
+def monkysort(data):
+    """
+    Shuffle data until sorted
+    :param data: list - data that mast be sorted
+    :return: data sorted my monkey algorithm
+    """
+    while not is_sorted(data):
+        random.shuffle(data)
+    return data
+
+monkeymean = [] # average sorting time
+monkeysd = [] # standard deviation of sorting time
+size = []
+progon = 15 # number of repetition for the same data size
+for i in range(2, 10000, 50):
+    timepr = []
+    size.append(i)
+    for j in range(0, progon):
+        data = np.random.randint(1, 100000, i)
+        start = time.time()
+        monkysort(data)
+        stop = time.time()
+        timepr.append(stop - start)
+    timemean = np.mean(timepr)
+    timesd = np.var(timepr)**0.5
+    monkeymean.append(timemean)
+    monkeysd.append(timesd)
+
+plt.title("Monkey time")
+plt.scatter(size, monkeymean, c = 'green', label='mean time' )
+plt.scatter(size, monkeysd, c = 'pink', label='sd of time')
+plt.xlabel('sample size')
+plt.ylabel('time')
+plt.legend()
+
 
 ## Task 4. Визуализируйте random walk (случайная прогулка, да)) в 2-мерном пространстве, где вы начинаете в (0, 0)
 # и можете перемещаться вверх, вниз, вправо и влево Как визуализировать - скаттерплот, где по x - x, а по y - y (10 баллов)
 ## не случайная прогулка, а случайное блуждание!
 
-## Task 5. Сгенерируйте и нарисуйте треугольник Серпинского (15 баллов)
+# defining the number of steps
+n = 10000
+# creating two array for containing x and y coordinate of size equals to the number of size and filled up with 0's
+x = np.zeros(n)
+y = np.zeros(n)
+# filling the coordinates with random variables
+for i in range(1, n):
+    val = random.randint(1, 4)
+    if val == 1:
+        x[i] = x[i - 1] + 1
+        y[i] = y[i - 1]
+    elif val == 2:
+        x[i] = x[i - 1] - 1
+        y[i] = y[i - 1]
+    elif val == 3:
+        x[i] = x[i - 1]
+        y[i] = y[i - 1] + 1
+    else:
+        x[i] = x[i - 1]
+        y[i] = y[i - 1] - 1
+# plotting the graph
+plt.title("Random walk ($n = " + str(n) + "$ steps)")
+plt.scatter(x, y, s=1, c = 'green')
+## График прикладывать? Там красота :)
 
+
+## Task 5. Сгенерируйте и нарисуйте треугольник Серпинского (15 баллов)
+# пока не получилось! :( сделаю обязательно!
 
 ## Task 6. *Сделайте программу, получающую на вход текст, и выдающую этот же текст со следующими изменениями - буквы
 # во всех словах кроме первой и последней перемешаны. Для простоты пока будем считать, что пунктуации нет.
