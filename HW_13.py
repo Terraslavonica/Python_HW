667a99c97b5a34b53ddaf7536cfba0fa312b5499
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-03-29 18:00:57 +0300

    not full but better

--- HW_13.py
+++ HW_13.py
@@ -1,21 +1,38 @@
 from Bio.Alphabet import generic_rna
 from Bio.Seq import Seq
+from numbers import Number
+
 
 # Tesk 1. Сделайте небольшой класс, описывающий какую-то сущность на ваш выбор (организм/тип данных/что-то ещё).
 # В классе должен быть конструктор и пара методов (10 баллов)
 
 class MoneyBox: ## Можно я тут заюзаю мировую задачу со стеика? Мне понадобилось, наеврное, полчаса, чтобы эта простейшая задача решилась
     def __init__(self, capacity):
+        """
+        Class MoneyBox
+        :param capacity: the volume of MoneyBox, which is empty at the beginning
+        """
         self.count = 0
         self.capacity = capacity
 
     def can_add(self, v):
+        """
+        This function help to check if we can add to the MoneyBox v coins
+        :param v: int - number of coins
+        :return: bul - True if we can put v coins (have enough space in the MoneyBox), and False if not
+        """
         if self.count + v <= self.capacity:
             return True
         else:
-            return False
+            return False # Здесь нужен else потому что нам нужно знать в явном виде, можем ли мы добавть сумму в скобках в копилку
+                        # если мы уберем else, то ничего не будем получать при добавлении в копилку недопустимой суммы
 
     def add(self, v):
+        """
+        Add v coins to our MoneyBox if we have enough space in the MoneyBox
+        :param v: int - number of coins
+        :return: total number of coins in the MoneyBox
+        """
         if self.can_add(v):
             self.count += v
         # return self.count
@@ -44,38 +61,60 @@ class RnaClass:
         :param table: The Genetic Code Table
         :return: protein sequence
         '''
-        self.tl = self.rnaseq.translate(table=table)
-        return self.tl
+        self.protseq = self.rnaseq.translate(table=table)
+        return self.protseq
 
     def bts(self):
         '''
         Function making back translation, i.e. cDNA from RNA sequence
         :return: cDNA sequence
         '''
-        self.bts = self.rnaseq.back_transcribe()
-        return self.bts
+        self.back = self.rnaseq.back_transcribe()
+        return self.back
 
 
 x = RnaClass("AUGAUAGCAGUU")
 x.rnaseq
 
-# Нормально, что три записи ниже дают ожно и то же?
 x.tl()
-x.tl
 RnaClass.tl(x)
 
 x.bts()
 x.bts
 
-y = RnaClass('AGTACACTGGT')
+y = RnaClass('AGUACACUGGUU')
 y.tl()
-y.tl
 y.bts()
-y.bts
+
 
 
 # Task 3.  Напишите класс, унаследовавшись от сэтов, который будет содержать в себе только положительные числа при
 # создании и не будет добавлять неположительные элементы (подсказка - методы конструктора и add) (10 баллов)
+
+class PositiveSet(set):
+
+    def __init__(self, vals = ''):
+        super().__init__()
+        self.vals = vals
+        for val in self.vals:
+            if isinstance(val, Number) and val > 0:
+                super().add(val)
+
+    def add(self, other):
+        if isinstance(other, Number) and other > 0:
+            self.update([other])
+
+d = PositiveSet([10,2,3])
+print(d) # PositiveSet({3, 10, 2})
+f = PositiveSet()
+f.add(3)
+f.add(-4)
+f.add('may')
+print(f) # PositiveSet({3})
+# but!!!
+f.update('may')
+print(f) # PositiveSet({'m', 3, 'a', 'y'}) # как я поняла, так задумано. Если нет, переписать апдейт можно аналогично add
+
 
 
 # Task 4. Создайте класс для сбора статистик по фастам. Входные параметры: # путь к фаста файлу
