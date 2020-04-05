c1df90591e289440b8c9a9109159abb201d0adf0
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-04-05 13:36:59 +0300

    HW_14 Task 2 and 3

---
+++ HW_14.py
@@ -0,0 +1,18 @@
+## Task 1. Зарегистрируйтесь на кэггле
+# done: https://www.kaggle.com/terraslavonica/account
+
+
+## Task 2. В файлике train.csv содержится информация о числе ридов с каждым из 4-ёх нуклеотидов
+# по разным позициям (колонки A, T, G, C)). Постройте гистограмму распределения этих чисел (7 баллов)
+
+# See HW_14_jupyter
+
+## Task 3. Сохраните в файл train_part.csv следующую часть из файла train.csv:
+# строки, где matches больше чем среднее
+# колонки pos, reads_all, mismatches, deletions, insertions
+
+# See HW_14_jupyter
+# See train_part.csv
+
+## Task 4. Проведите Explorative Data Analysis знаменитого датасэта титаника
+# (или какого-нибудь другого, если вам скушно с Титаником). Нужны корреляции, графики распределений
