0f9ee0c8519ba94728b2d159d924b310cc677acf
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-04-09 20:03:17 +0300

    Fixed

--- HW_15.py
+++ HW_15.py
@@ -5,33 +5,29 @@ import matplotlib.pyplot as plt
 
 # Task 1. Распарсите файл references и запишите оттуда все ftp ссылки в файл ftps (5 баллов)
 pat = re.compile('ftp[\.a-zA-Z0-9_#\/@:]+')  # ftp://[user[:password]@]host[:port]/url-path
-with open('references.txt', 'r') as file:
-    with open('ftps.txt', 'w') as sklad:
+with open('references.txt', 'r') as file, open('ftps.txt', 'w') as sklad:
         for string in file:
-            for m in pat.findall(string):
-                sklad.write(m)
-                sklad.write("\n")
+            sklad.write('\n'.join(pat.findall(string)))
 
 
+# Займёмся парсингом рассказа в файле 2430AD!
 
-# Task 2. Займёмся парсингом рассказа в файле 2430AD!
-
-## 1  Для начала извлеките оттуда все числа (3 балла)
-pat = re.compile('[0-9]+')
+## 2  Для начала извлеките оттуда все числа (3 балла)
+pat = re.compile('\d+\.?\d+')
 with open('2430AD.txt', 'r') as file:
     for string in file:
         if pat.findall(string):
             print(pat.findall(string)) # куда извлечь - не сказано, так что так
 
 
-## 2 Теперь выберите из текста все слова, в которых есть буква a, регистр при этом не важен (5 баллов)
+## 3 Теперь выберите из текста все слова, в которых есть буква a, регистр при этом не важен (5 баллов)
 pat2 = re.compile(r'[a-zA-Z]*a[a-zA-Z]*', re.I)
 with open('2430AD.txt', 'r') as file:
     for string in file:
         if pat2.findall(string):
             print(pat2.findall(string))
 
-## 3 Далее извлеките все восклицательные предложения (10 баллов)
+## 4 Далее извлеките все восклицательные предложения (10 баллов)
 pat3 = re.compile(r'[A-Z].+?!')
 with open('2430AD.txt', 'r') as story:
     for i in story:
@@ -47,7 +43,7 @@ with a certain amount of real sympathy, too), "I know. There was once a time!', 
 ['If we succeed!']
 '''
 
-## 4 А теперь построим гистограмму распределения длин уникальных слов (без учёта регистра, длина от 1) в тексте (15 баллов)
+## 5 А теперь построим гистограмму распределения длин уникальных слов (без учёта регистра, длина от 1) в тексте (15 баллов)
 
 def flatten(List):
     """
@@ -92,7 +88,7 @@ plt.show()
 
 
 
-# Task 3. Ну и, куда же без этого - паттерн для поиска email адресов) (10 баллов)
+# Task 6. Ну и, куда же без этого - паттерн для поиска email адресов) (10 баллов)
 pattern = re.compile(r'[a-zA-Z0-9_\.-]+@[a-z\.]*\.[a-z]*')
 emails = str('Terra8908@yandex.ru, e.u.yakovleva@gmail.com, akjd-470@mail.eu, eyakovleva@econ.msu.ru, Hellow world')
 pattern.findall(emails)
