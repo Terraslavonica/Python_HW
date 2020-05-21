d22bae77028dff01e29bde10fd1054a4e4e22e15
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-05-21 14:04:25 +0300

    My trimmomatic fixed and added

--- HW_20_Task_2_Trimm.py
+++ HW_20_Task_2_Trimm.py
@@ -1,20 +1,16 @@
 from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqIO
-from graphviz import Graph
-import networkx as nx
-import operator
 import argparse
-
 
 # Argparse part
 parser = argparse.ArgumentParser(prog='my_trimmomatic', description='Awesome my own trimmomatic')
 parser.add_argument('reads_input', type=str, metavar='reads', help='fastq file with reads')
-parser.add_argument('-q', '--qual_lim', default = 30, type=int, metavar='INT', help='quality threshold')
-parser.add_argument('-o','-output', default = 'trim_out.fastq', type=str, metavar='fastq_name', help='fastq file with trimmed reads')
+parser.add_argument('-b', '--begin', default=0, type=int, metavar='INT',
+                    help='number of nucleotides that should be clipped at the begining of the read')
+parser.add_argument('-f', '--finish', default=0, type=int, metavar='INT',
+                    help='number of nucleotides that should be clipped at the end of the read')
+parser.add_argument('-q', '--qual_lim', default=30, type=int, metavar='INT', help='quality threshold')
+parser.add_argument('-o', '-output', default='trim_out.fastq', type=str, metavar='fastq_name',
+                    help='fastq file with trimmed reads')
 
 args = parser.parse_args()
 args = args.__dict__
@@ -24,25 +20,31 @@ args = args.__dict__
 ## Сделайте небольшой аналог триммоматика, который может отрезать нуклеотиды в начале последовательностей, в конце,
 # удалять фрагменты с концов со средним качеством ниже указанного (25 баллов)
 
-def my_trimmer(fastq_file, limqual = 30, trim_fastq_file='trim_out.fastq'):
-    trim_rec = [] # list for trimmed records
+def my_trimmer(fastq_file, limqual=30, begin=0, finish=0, trim_fastq_file='trim_out.fastq'):
+    trim_rec = []  # list for trimmed records
     fastq = SeqIO.parse(fastq_file, "fastq")
     for record in fastq:
         rec_len = len(record)
+        # ends clipping
+        clip_record = record[begin:(rec_len-finish)]
+        # clipping by quality
         start = 0
-        stop = rec_len
-        for i in range(rec_len//2 + 3):
-            qual = record.letter_annotations["phred_quality"][0:i+1]
-            if sum(qual)/len(qual) < limqual:
+        clip_rec_len = len(clip_record)
+        stop = clip_rec_len
+        for i in range(clip_rec_len // 2 + 3):
+            qual = clip_record.letter_annotations["phred_quality"][0:i + 1]
+            if sum(qual) / len(qual) < limqual:
                 start = i
-        for j in range(rec_len//2 + 3):
-            rev_record = record[::-1]
-            qual = rev_record.letter_annotations["phred_quality"][0:j+1]
-            if sum(qual)/len(qual) < limqual:
-                stop = rec_len - j
-        trim_rec.append(record[start:stop])
+        for j in range(clip_rec_len // 2 + 3):
+            rev_record = clip_record[::-1]
+            qual = rev_record.letter_annotations["phred_quality"][0:j + 1]
+            if sum(qual) / len(qual) < limqual:
+                stop = clip_rec_len - j
+        trim_rec.append(clip_record[start:stop])
         SeqIO.write(trim_rec, trim_fastq_file, 'fastq')
     return f'результат записан в файл {trim_fastq_file}'
 
-result = my_trimmer(args['reads_input'], args['qual_lim'], args['o'])
+# my_trimmer('Eriparia_1_10K.fq', 30, 10, 10, 'new_trim.fastq')
+
+result = my_trimmer(args['reads_input'], args['begin'], args['finish'], args['qual_lim'], args['o'])
 print(result)
