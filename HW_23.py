3821ef28884a2761afae231a4d42c430374e8cfd
Ekaterina Yakovleva <e.u.yakovleva@gmail.com>
2020-05-26 12:06:50 +0300

    Fixed path and layers

--- HW_23.py
+++ HW_23.py
@@ -6,9 +6,10 @@ import numpy as np
 # К этим заданиям нужен код и результаты)
 
 # Task 0. Наделала кучу грибных фоток и выложила в соответствующей папке на гите
-me = plt.imread('KateY.png')
+path = 'KateY.png'
+me = plt.imread(path)
 me.shape
-ya = me[:, :, :-1]
+ya = me[:, :, 0:3]
 ya.shape
 
 plt.imshow(ya)
@@ -22,75 +23,75 @@ plt.axis('off')
 plt.show()
 plt.savefig('blue_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 ya[:, :, [0,2]] = 0
 plt.imshow(ya)
 plt.axis('off')
 plt.show()
 plt.savefig('green_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 ya[:, :, [1,2]] = 0
 plt.imshow(ya)
 plt.axis('off')
 plt.show()
 plt.savefig('red_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='gray')
 plt.axis('off')
 plt.show()
 plt.savefig('gray_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='inferno')
 plt.axis('off')
 plt.show()
 plt.savefig('inferno_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='terrain')
 plt.axis('off')
 plt.show()
 plt.savefig('terrain_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='flag')
 plt.axis('off')
 plt.show()
 plt.savefig('flag_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='prism')
 plt.axis('off')
 plt.show()
 plt.savefig('prism_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='gist_earth')
 plt.axis('off')
 plt.show()
 plt.savefig('gist_earth_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='gnuplot')
 plt.axis('off')
 plt.show()
 plt.savefig('gnuplot_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='cubehelix')
 plt.axis('off')
 plt.show()
 plt.savefig('cubehelix_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='gist_ncar')
 plt.axis('off')
 plt.show()
 plt.savefig('gist_ncar_me.png')
 
-ya = plt.imread('KateY.png')[:, :, :-1]
+ya = plt.imread(path)[:, :, 0:3]
 plt.imshow(ya.mean(2), cmap='rainbow')
 plt.axis('off')
 plt.show()
@@ -98,8 +99,9 @@ plt.savefig('rainbow_me.png')
 
 
 # Task 1. Применить 5 фильтров к своей фотке (15 баллов)
-img = Image.open('Kate_start.PNG') #.convert('L')
-data = np.array(img)[:, :, :-1]
+path = 'Kate_start.PNG'
+img = Image.open(path) #.convert('L')
+data = np.array(img)[:, :, 0:3]
 img.show()
 
 # plt.imshow(data)
@@ -240,7 +242,8 @@ plt.show()
 plt.savefig('Task_1_kernel_8.png')
 
 # Task 2. Сделать фильтры 3 x 3 и 5 x 5, ищущие кружочки, применить их (10 баллов)
-rings = Image.open('rings.PNG')
+path = 'rings.PNG'
+rings = Image.open(path)
 rings.show()
 data = np.array(rings)[:, :, :-1]
 data
